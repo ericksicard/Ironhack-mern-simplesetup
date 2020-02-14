221252994a8fa3eca9dca370c18f3e8f4a35bc89
Erick Sicard <esrfree@gmail.com>
2020-02-14 16:29:13 -0500

    Mongoose setup and connection to MongoDB

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
 import path from 'path'
 import express from 'express'
 import { MongoClient } from 'mongodb'
+import mongoose from 'mongoose'
 import template from './../template'
 import config from './../config/config'
 import app from './express'
 //comment out before building for production
 import devBundle from './devBundle'
 
-//const app = express()  --> created at express.js
+//const app = express()  --> now created at express.js
 
 //comment out before building for production
 devBundle.compile(app)
@@ -26,10 +27,18 @@ app.listen(config.port, (err) => {
   console.info('Server started on port %s.', config.port)
   })
 
+// Setting up Mongoose
+mongoose.Promise = global.Promise
+mongoose.connect(config.mongoUri)
+
+mongoose.connection.on('error', () => {
+  throw new Error(`unable to connect to database: ${mongoUri}`)
+  })
+
 // Database Connection URL
-const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/mernSimpleSetup'
+//const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/mernSimpleSetup'  --> now using config.js
 // Use connect method to connect to the server
-MongoClient.connect(url, (err, db)=>{
+MongoClient.connect(config.mongoUri, (err, db)=>{
   console.log("Connected successfully to mongodb server")
   db.close()
 })
