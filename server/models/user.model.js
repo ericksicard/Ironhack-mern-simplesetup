b61053e4e9d87111f7a32190de8d8bf8524f91ba
Erick Sicard <esrfree@gmail.com>
2020-02-14 17:49:02 -0500

    Passport as a virtual field, encryption and authentication

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -26,3 +26,37 @@ const userSchema = new mongoose.Schema(
         salt: String
     }
 )
+
+/******* change to bcryptjs if possible ************/
+// Pasword for auth as a virtual field
+userSchema
+    .virtual('password')
+    .set(function(password) {
+        this._password = password;
+        this.salt = this.makeSalt();
+        this.hashed_password = this.encryptPassword(password)
+    })
+    .get(function() {
+        return this._password;
+    })
+
+// Encryption and authentication
+userSchema.methods = {
+    authenticate: function(plainText) {
+        return this.encryptPassword(plainText) === this.hashed_password;
+    },
+    encryptPassword: function(password) {
+        if( !password ) return ''
+        try {
+            return crypto
+                .createHmac('sha1', this.salt)
+                .update(password)
+                .digest('hex')
+        } catch( err ) {
+            return ''
+        }
+    },
+    makeSalt: function() {
+        return Math.round((new Date().valueOf() * Math.random())) + ''
+    }
+}
