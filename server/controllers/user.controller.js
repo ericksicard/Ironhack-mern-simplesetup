8fff3d3f2c320192d5ab53653138ac1580d234ff
Erick Sicard <esrfree@gmail.com>
2020-02-15 22:46:19 -0500

    backend ready and tested

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -9,7 +9,7 @@ const create = (req, res, next) => {
     const user = new User (req.body)
     user.save( (err, result) => {
         if (err) {
-            return res.status(400).jason({
+            return res.status(400).json({
                 error: errorHandler.getErrorMessage(err)
             })
         }
